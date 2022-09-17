6e47b8a90ef17cf48f827dc4a54adfcb8afa0ee5
Zalerys <gustaveconstans@gmail.com>
2022-09-18 00:31:00 +0200

    Creation  Page Authorization brand

--- crushon-app/src/components/AuthorizationM/Authorization.js
+++ crushon-app/src/components/AuthorizationM/Authorization.js
@@ -1,7 +1,61 @@
-import { React } from "react";
+import { React,  useState } from "react"
+import "./Authorization.css"
+
+
+
+// const Authorizationbrand = () => {
+//   return(
+//     <div>
+
+//     </div>
+//   )
+// }
+
 
 function Authorization() {
-  return <div>test author</div>;
+
+  const [] = useState([])
+  const [] = useState([]);
+
+  const showbrand = () => {
+    document.getElementById("newbrand").style.display="flex"
+  }
+
+  return (
+    <div>
+      <div>
+        <h1>Authorization of Brand</h1>
+      </div>
+      <div>
+        <button onClick={showbrand}>Add new brand</button>
+        <div id="newbrand">
+          <div>
+            <h3>New Brand</h3>
+          </div>
+          <div>
+            <h5>Name of the Brand</h5>
+            <input></input>
+          </div>
+          <div>
+            <h5>Authorization</h5>
+            <select name="Authorization" id="">
+              <option defaultValue hidden>
+                --Choose authorization--
+              </option>
+              <option value="authorized">Authorized</option>
+              <option value="prohibited">Prohibited</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h3>List of authorized brand</h3>
+      </div>
+      <div>
+        <h3>List of prohibited brand</h3>
+      </div>
+    </div>
+  );
 }
 
 export default Authorization;
